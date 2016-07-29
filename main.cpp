b9265c08fb28164baacfc9b9465cedd93ae19366
Pavel Pavlov <mtlroom@hotmail.com>
2016-07-29 12:39:54 -0400

    improved tests precision

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ void TEST(const char *expr, double expected_res)
     assert(parse_result);
     double res = eval(parser);
     char str[256];
-    sprintf(str, "%f %f", res, expected_res);
+    sprintf(str, "%.10f %.10f", res, expected_res);
     double d1, d2;
     sscanf(str, "%lf %lf", &d1, &d2);
     if (d1==d2)
@@ -28,9 +28,10 @@ int main()
     TEST("1.0/3", 0.33333333333);
     TEST("-0.123/(3+log(100))log(10)(1+1*5/6(3*2))", -0.1476);
     TEST("(0.1/3+.1/3+10/300)/0.001e10", 1e-8);
-    TEST("(0.1/3+.1/3+10/300+3.14159265359)/1e8+10", 10.000000032);
-    TEST("log((((((0.1/3+.1/3+10/300+3.14159265359)/1e8+10)))))", 1.00000000141);
-    TEST("-5log((((((0.1/3+.1/3+10/300+3.14159265359)/1e8+10)))))(0+.1)", -0.5000000007);
+    TEST("(0.1/3+.1/3+10/300+3.14159265359)/1e8+10", 10.0000000324);
+    TEST("log((((((0.1/3+.1/3+1/30+3.14159265359)/1e8+10)))))", 1.00000000141);
+    TEST("-5log(((((.2/3+1/3+3.1415926535)/1e8+10))))(0+.1)", -0.50000000076);
+    TEST("-5log(((((.2/3+1/3+3.1415926535)/1e8+10))))(0+.1)+1", 0.49999999923);
     if (err_count)
         printf("%d tests passed, %d tests failed\n", ok_count, err_count);
     else
